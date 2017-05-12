da3a1e1b1cd3882af668947896f4ad6b1a559192
Pramod Shenoy <PramodShenoy@users.noreply.github.com>
2017-05-12 15:44:35 +0000

    nested,break and continue

--- nested.go
+++ nested.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a int=5
+	var a int=10
 	var b string="*"
 
 	for i := a; i >=0; i-- {
@@ -12,4 +12,17 @@ func main() {
 		}
 		fmt.Println()
 	}
+
+
+	for i := 0; i <=a; i++ {
+		for j := 0; j<=i ; j++ {
+			if i==3 {
+				continue;//or break
+			}
+			fmt.Printf(b)
+		}
+		fmt.Println()
+	}
+
+
 }
